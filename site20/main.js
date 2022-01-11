59bb98f40ef720fa939bb799c2c554601173a903
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-11 17:41:48 +0200

    js

---
+++ site20/main.js
@@ -0,0 +1,26 @@
+console.log('test');
+alert ('Hello, World (da)')
+    let bugaga = 'obamium'
+    let proger = 'ya proger'
+    console.log('my name is ' + bugaga + ', cool, yea? ' + proger)
+//     function othername() {
+//         var input = document.getElementById("userInput").value;
+//         alert('my name is ' + input);
+//     }
+//     let button = document.querySelector('button');
+
+// function greet() {
+//   let name = prompt('What is your name?');
+//   alert(`Hello ${name}, nice to see you!`);
+// }
+let mememe = prompt('Кто ты?')
+console.log('my name is ' + mememe)
+confirm ('да?')
+// let a = 69
+// let b = 420
+let a = Number (prompt('a?'))
+let b = Number (prompt('b?'))
+// let C = console.log (b)
+console.log(a + b)
+// console.log(C + b)
+console.log (('b' + 'a' + + 'a' + 'a').toLowerCase())
