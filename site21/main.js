779a12c248d8114c4563a4f9f80363ae3e03fa41
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-20 19:27:15 +0200

    gipo

--- site21/main.js
+++ site21/main.js
@@ -1,72 +1,71 @@
-let name = +prompt("Throw me number a");
-
+let name = +prompt("Throw me number a")
 function enterb() {
-    let b = +prompt("Throw me number b");
+    b = +prompt("Throw me number b")
     console.log(b)
-    var el = document.getElementById('name');
+    var el = document.getElementById('name')
 if (typeof el.innerText !== 'undefined') {
     // IE8-
-    el.innerText = 'a=' + name + ', b=' + b;
+    el.innerText = 'a=' + name + ', b=' + b
 } else {
     // Нормальные браузеры
-    el.textContent = 'a=' + name + ', b=' + b;
+    el.textContent = 'a=' + name + ', b=' + b
 }
-var el = document.getElementById('our');
+var el = document.getElementById('our')
 if (typeof el.innerText !== 'undefined') {
     // IE8-
-    el.innerText = 'Наши Числа';
+    el.innerText = 'Наши Числа'
 } else {
     // Нормальные браузеры
-    el.textContent = 'Наши Числа';
+    el.textContent = 'Наши Числа'
 }
 }
 
-var el = document.getElementById('name');
+var el = document.getElementById('name')
 if (typeof el.innerText !== 'undefined') {
     // IE8-
-    el.innerText = name;
+    el.innerText = name
 } else {
     // Нормальные браузеры
-    el.textContent = name;
+    el.textContent = name
 }
 
 function clicka() {
 nametwo = name**2
-var elnew = document.getElementById('nametwo');
+var elnew = document.getElementById('nametwo')
 if (typeof elnew.innerText !== 'undefined') {
     // IE8-
-    elnew.innerText = nametwo;
+    elnew.innerText = nametwo
 } else {
     // Нормальные браузеры
-    elnew.textContent = nametwo;
+    elnew.textContent = nametwo
 }
-var elnew = document.getElementById('nametwob');
+var elnew = document.getElementById('nametwob')
 if (typeof elnew.innerText !== 'undefined') {
     // IE8-
-    elnew.innerText = nametwo;
+    elnew.innerText = nametwo
 } else {
     // Нормальные браузеры
-    elnew.textContent = nametwo;
+    elnew.textContent = nametwo
 }}
 function clickb() {
     nameb = -name
-    var elnew = document.getElementById('nameb');
+    var elnew = document.getElementById('nameb')
     if (typeof elnew.innerText !== 'undefined') {
         // IE8-
-        elnew.innerText = nameb;
+        elnew.innerText = nameb
     } else {
         // Нормальные браузеры
-        elnew.textContent = nameb;
+        elnew.textContent = nameb
     }}
     function clickc() {
         namec = name % 10
-        var elnew = document.getElementById('namec');
+        var elnew = document.getElementById('namec')
         if (typeof elnew.innerText !== 'undefined') {
             // IE8-
-            elnew.innerText = namec;
+            elnew.innerText = namec
         } else {
             // Нормальные браузеры
-            elnew.textContent = namec;
+            elnew.textContent = namec
         }}
         function clickd() {
             named = name / 3
@@ -74,28 +73,38 @@ function clickb() {
             var elnew = document.getElementById('named');
             if (typeof elnew.innerText !== 'undefined') {
                 // IE8-
-                elnew.innerText = named;
+                elnew.innerText = named
             } else {
                 // Нормальные браузеры
-                elnew.textContent = named;
+                elnew.textContent = named
             }}
             function clicke() {
                 namee = name * 4
                 var elnew = document.getElementById('namee');
                 if (typeof elnew.innerText !== 'undefined') {
                     // IE8-
-                    elnew.innerText = namee;
+                    elnew.innerText = namee
                 } else {
                     // Нормальные браузеры
-                    elnew.textContent = namee;
+                    elnew.textContent = namee
                 }}
                 function clickf() {
                     namef = name ** 2
-                    var elnew = document.getElementById('namef');
+                    var elnew = document.getElementById('namef')
                     if (typeof elnew.innerText !== 'undefined') {
                         // IE8-
-                        elnew.innerText = namef;
+                        elnew.innerText = namef
                     } else {
                         // Нормальные браузеры
-                        elnew.textContent = namef;
+                        elnew.textContent = namef
                     }}
+                    function clickg() {
+                        nameg = Math.sqrt(name ** 2 + b ** 2)
+                        var elnew = document.getElementById('nameg')
+                        if (typeof elnew.innerText !== 'undefined') {
+                            // IE8-
+                            elnew.innerText = nameg
+                        } else {
+                            // Нормальные браузеры
+                            elnew.textContent = nameg
+                        }}
