1182f0dd4f3c1eca0e4fad3fa8eec19660bd5371
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-23 19:44:23 +0200

    elevator game fixed

--- site23/main.js
+++ site23/main.js
@@ -37,11 +37,12 @@ function move1() {
  }
 async function clicke() {
      if (danger == 1) {
+     if ( die != 1 ) {
      die=1
      none.style.display= "none"
      gg.style.opacity= "0"
      elevator.style.marginTop="35px"
-     var image = document.getElementById("imageid")
+     let image = document.getElementById("imageid")
      image.src = "l2.png"
      for (let i = 35; i < 330; i++) {
           await sleep(1);
@@ -49,7 +50,7 @@ async function clicke() {
      }
      image.src = "die.png"
      elevator.style.marginTop="380px"
-     } else alert('Мимо!')
+     }} else alert('Мимо!')
  }
  setInterval(move1, 1200);
  setInterval(move2, 2700);
