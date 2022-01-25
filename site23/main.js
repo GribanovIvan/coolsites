c52aee0dc67b84b1a9267bdbb661885d59c6034f
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-25 14:53:39 +0200

    games

--- site23/main.js
+++ site23/main.js
@@ -56,3 +56,87 @@ async function clicke() {
  setInterval(move2, 2700);
  setInterval(move3, 3100);
  setInterval(goin, 2500);
+ let cube=document.querySelector('.ultcube')
+ let martop=0
+ let marleft=0
+ let cw=200
+ let ch=200
+ let step=10
+ function down() {
+martop += step
+if ( martop + ch > 500 ) {
+    alert('куда.')
+    martop -= step
+}
+cube.style.marginTop= martop + "px"
+ }
+ function up() {
+    martop -= step
+    if ( martop < 0 ) {
+        alert('куда.')
+        martop=0
+    }
+    cube.style.marginTop= martop + "px"
+     }
+     function right() {
+        marleft += step
+        if ( marleft + cw > 700 ) {
+            alert('куда.')
+            marleft -= step
+        }
+        cube.style.marginLeft= marleft + "px"
+         }
+         function left() {
+            marleft -= step
+            if ( marleft < 0 ) {
+                alert('куда.')
+                marleft=0
+            }
+            cube.style.marginLeft= marleft + "px"
+             }
+             function wmin() {
+                cw -= step
+                if ( cw < 200 ) {
+                    alert('cw must be >= 200')
+                    cw=200
+                }else 
+                cube.style.width= cw + "px"
+                 }
+         
+                 function wup() {
+                    cw += step
+                    if ( marleft + cw > 700 ) {
+                        alert('куда.')
+                        cw -= step
+                    }
+                    if ( cw > 600 ) {
+                        alert('cw must be < 600')
+                        cw=600
+                    }else 
+                    cube.style.width= cw + "px"
+                     }
+             
+             function hdown() {
+                ch -= step
+                if ( ch < 100 ) {
+                    alert('ch must be >= 100    ')
+                    ch=100
+                }else 
+                cube.style.height= ch + "px"
+                 }
+         
+                 function hup() {
+                    ch += step
+                    if ( martop + ch > 500 ) {
+                        alert('куда.')
+                        ch -= step
+                    }
+                    if ( ch > 400 ) {
+                        alert('ch must be < 400')
+                        ch=400
+                    }else 
+                    cube.style.height= ch + "px"
+                     }
+function stepe() {
+    step=+prompt('step?')
+}
