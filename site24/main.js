9c15aedcce9aade3dddf217f6c72112d2b062b98
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-25 17:22:57 +0200

    strahchrome

--- site24/main.js
+++ site24/main.js
@@ -5,5 +5,6 @@ let num=1
 enter.onclick = function() {
     msg=    msg + '<li >' + num + '. ' + document.getElementById("input-id").value + '</li>'
     num++
+    document.getElementById("input-id").value=''    
     text.innerHTML = msg
 }
