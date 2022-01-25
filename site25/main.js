b67dbdce1e36be5318411d3728917ad03aaa6b49
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-25 18:03:44 +0200

    site25

--- site25/main.js
+++ site25/main.js
@@ -5,3 +5,28 @@ for (let i = 1; i <= 10; i++) {
     i++
     console.log(i)
 }
+let n=+prompt('n?')
+num=2
+loop:for (let i = 2; i <= n; i++) {
+for (let temp = 2; temp < i; temp++) {
+if (i % temp == 0) continue loop;
+}
+console.log(i)
+}
+let a=+prompt('a?')
+let b=+prompt('b?')
+function min(a,b) {
+    if ( a > b) {
+        actual=b
+    } else {
+    if ( b > a) {
+        actual=a
+    } else{actual='error'}}
+    return actual
+}
+alert('min=' + min(a,b))
+let x=+prompt('x?')
+function pow(x,n) {
+    return x**n
+}
+alert ('x**n=' + pow(x,n))
