81c8b003332d1d08b7524fa2cbb20a164ca2f8de
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-28 16:25:41 +0200

    gentoo

---
+++ site26/main.js
@@ -0,0 +1,5 @@
+let task3 = () => {age=+prompt('Сколько вам лет?')
+        if (age < 18) {
+            alert ('А нельзя!')
+        } else {if (age >= 18 )
+            {window.location='https://wiki.gentoo.org/wiki/Handbook:AMD64/ru'}}}
