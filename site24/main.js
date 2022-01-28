d66f6979c275e0b5545e9fc6793952293c540955
GribanovIvan <van.a.gribanov@gmail.com>
2022-01-28 17:54:19 +0200

    wow

--- site24/main.js
+++ site24/main.js
@@ -1,10 +1,35 @@
 const text = document.getElementById("ptext")
-// text.innerText = 'test'
+const parent = document.getElementById("ptext")
+const trash = document.getElementById("ptextt")
 let msg=''
 let num=1
 enter.onclick = function() {
-    msg=    msg + '<li >' + num + '. ' + document.getElementById("input-id").value + '</li>'
+    msg=msg + '<li id=li' + num + '>' + document.getElementById("input-id").value + '<button onclick=del(' + num + ')>del</button></li>'
     num++
     document.getElementById("input-id").value=''    
     text.innerHTML = msg
+}
+function reverseString(str) {
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+function del (n) {
+    let child=document.getElementById("li" + n)
+    trash.appendChild(child);
+    // parent.removeChild(child);
+    msg=text.innerHTML
+    child.innerHTML = reverseString((reverseString(child.innerHTML).substr(20))) + 'realdel(' + n + ')">realdel</button> <button onclick="add(' + n + ')">add</button>'
+}
+function realdel (n) {
+    trash.removeChild(document.getElementById("li" + n))
+}
+function add (n) {
+    child=document.getElementById("li" + n)
+    let fakenum=num-1
+    console.log (reverseString(reverseString(child.innerHTML).substr(66)) + 'del(' + fakenum + ')">del</button></li>')
+    child.innerHTML = reverseString(reverseString(child.innerHTML).substr(66)) + 'del(' + fakenum + ')">del</button></li>'
+    parent.appendChild(child)
 }
--- site26/main.js
+++ site26/main.js
@@ -3,3 +3,12 @@ let task3 = () => {age=+prompt('Сколько вам лет?')
             alert ('А нельзя!')
         } else {if (age >= 18 )
             {window.location='https://wiki.gentoo.org/wiki/Handbook:AMD64/ru'}}}
+let text = document.getElementById("add")
+let msg=''
+function addblock () {
+    msg=msg + '<div class="block"></div>'
+    text.innerHTML = msg
+}
+addblock()
+addblock()
+addblock()
