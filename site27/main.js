17775ceb10a951a6bd8892b5735a495b33e8c1a8
GribanovIvan <van.a.gribanov@gmail.com>
2022-02-01 17:13:17 +0200

    colors!

--- site27/main.js
+++ site27/main.js
@@ -4,9 +4,12 @@ const block = document.querySelector(".block")
 block.style.width='200px'
 block.style.height='200px'
 block.style.background='red'
-let array = ['red', 'green', 'violet']
+let array = ['#FF0000', '#00FF00', '#0000FF']
 let temp=0
 change.onclick = function() {
+    let x = document.getElementById("add").value
+    console.log(x)
+    if (!array.includes(x)) {array.push(x)}
     temp=temp + 1
     block.style.background=array[temp % array.length]
 }
