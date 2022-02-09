12ca3c72f33ae54b30ec60d5a2abde30eb31e60e
GribanovIvan <van.a.gribanov@gmail.com>
2022-02-09 23:18:25 +0200

    fixx

--- site24/main.js
+++ site24/main.js
@@ -7,8 +7,11 @@ const audio3 = new Audio('nado3.opus')
 const audio4 = new Audio('nado4.opus')
 let msg=''
 let num=1
+let data = Array();
 enter.onclick = function() {
-    msg=msg + '<li id=li' + num + '>' + document.getElementById("input-id").value + '<button onclick=del(' + num + ')>del</button></li>'
+    let temp=num // any unique variable
+    msg=msg + '<li id=li' + temp + '>' + document.getElementById("input-id").value + '<button onclick=del(' + temp + ')>del</button></li>'
+    data[temp]=document.getElementById("input-id").value
     num++
     document.getElementById("input-id").value=''    
     text.innerHTML = msg
@@ -25,23 +28,21 @@ function reverseString(str) {
 }
 function del (n) {
     let child=document.getElementById("li" + n)
+    let fakenum=child.attributes.id.nodeValue.substr(2)
     trash.appendChild(child);
     // parent.removeChild(child);
     msg=text.innerHTML
-    child.innerHTML = reverseString((reverseString(child.innerHTML).substr(20))) + 'realdel(' + n + ')">realdel</button> <button onclick="add(' + n + ')">add</button>'
+    child.innerHTML=data[fakenum] + '<button onclick="realdel(' + fakenum + ')">realdel</button> <button onclick="add(' + fakenum + ')">add</button>'
 }
 function realdel (n) {
     trash.removeChild(document.getElementById("li" + n))
 }
 function add (n) {
     child=document.getElementById("li" + n)
-    let fakenum=num-1
-    console.log (reverseString(reverseString(child.innerHTML).substr(66)) + 'del(' + fakenum + ')">del</button></li>')
-    child.innerHTML = reverseString(reverseString(child.innerHTML).substr(66)) + 'del(' + fakenum + ')">del</button></li>'
+    let fakenum=child.attributes.id.nodeValue.substr(2)
+    child.innerHTML=data[fakenum] + '<button onclick="del(' + fakenum + ')">del</button></li>'
     parent.appendChild(child)
 }
 // ffmpeg -i 13.\ Русское\ поле\ экспериментов.m4a -ss 429.5 -c opus -strict -2 -t 6.8 nado3.opus
 // ffmpeg -i 13.\ Русское\ поле\ экспериментов.m4a -ss 425.9 -c opus -strict -2 -t 3.2 nado2.opus
 // ffmpeg -i 13.\ Русское\ поле\ экспериментов.m4a -ss 422.7 -c opus -strict -2 -t 2.9 nado1.opus
-function dredel (n){del(n)}
-function arealdel(n){realdel(n)}
